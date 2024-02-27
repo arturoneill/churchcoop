a67639d47106c2c8d082f9741d59a0a687bad684
arturoneill <mobius0919@gmail.com>
2024-02-27 09:08:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,12 +36,7 @@ if(ios) {
   });
 
 
-    $("body").on("click", "a[data-href]", function() {
-    var href = $(this).data("href");
-    if (href) {
-        location.href = href;
-    }
-});
+
 
     
 }
